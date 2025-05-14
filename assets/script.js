a1ab474d2d8b6fa5aa8a5b408d3d2aa45b6c9031
Clenet <clenetclenet03@gmail.com>
2025-05-14 16:52:45 +0530

    created grids function

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function createGrid(size) {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+  const squareSize = 960 / size;
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("grid-square");
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+  }
+}
+
+createGrid(16);
